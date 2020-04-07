4dc28eb4b90ace2f9c10de59ec1c48e09784c430
azlan <mhdazlanabaziz@gmai.com>
2020-04-07 15:26:59 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let express = require('express');
+let chalk = require('chalk');
+let debug = require('debug')('app');
+let morgan = require('morgan');
+let path = require('path'); //this one doesn't need NPM install
+
+var app = express();//instantiate express
+
+//setting routes
+app.get('/',function(req,res){
+ //res.send('Hello from my library app'); //sending some text
+ //res.sendFile(__dirname + '/views/index.html');
+ res.sendFile(path.join(__dirname,'views/index.html'))
+})
+
+// app.use(morgan('combined'));
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname,'/public/')))// tell express we're goint tostick files in the dir; setting for css js dir
+//app.use('/css',express.static(path,join(__dirname,'node_modules/bootstrap/dist/css')));
+//app.use('/js',express.static(path,join(__dirname,'node_modules/bootstrap/dist/js')));
+//app.use('/jquery',express.static(path,join(__dirname,'node_modules/bootstrap/dist/jquery')));
+
+//get express running
+app.listen(3000, function(){
+    // console.log('listening on port' + chalk.redBright(' 3000'));
+    // console.log(`listening on port ${chalk.green('3000')}`);
+    debug(`listening on port ${chalk.green('3000')}`);
+});
